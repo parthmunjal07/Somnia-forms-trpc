fa3c2b23d2640641d9d0e01ccd6bf0b33a5d3e67
parthmunjal07 <parth.munjal07@gmail.com>
2026-05-27 00:02:29 +0530

    add proxy option to GoogleStrategy and update db:push script

--- apps/api/src/auth/passport.ts
+++ apps/api/src/auth/passport.ts
@@ -12,6 +12,7 @@ if (env.GOOGLE_OAUTH_CLIENT_ID && env.GOOGLE_OAUTH_CLIENT_SECRET) {
         clientID: env.GOOGLE_OAUTH_CLIENT_ID,
         clientSecret: env.GOOGLE_OAUTH_CLIENT_SECRET,
         callbackURL: `${env.BASE_URL}/api/auth/google/callback`,
+        proxy:true
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
